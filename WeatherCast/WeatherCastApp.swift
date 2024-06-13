09640a412522ca010c581196fc3b9acf299aafc6
luislajo <luislajoborderias11@gmail.com>
2024-06-13 10:52:43 +0200

    Initial Commit

---
+++ WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by luis lajo borderias on 13/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
