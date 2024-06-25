060fe4d9f85f85028e963f60ebe7c5448f63e4a2
luislajo95 <luis.lajo@icloud.com>
2024-06-25 11:23:23 +0200

    first commit

---
+++ WeatherCast/VisualCrossing.swift
@@ -0,0 +1,18 @@
+//
+//  VisualCrossing.swift
+//  WeatherCast
+//
+//  Created by luis lajo borderias on 14/6/24.
+//
+
+import SwiftUI
+
+struct VisualCrossing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisualCrossing()
+}
